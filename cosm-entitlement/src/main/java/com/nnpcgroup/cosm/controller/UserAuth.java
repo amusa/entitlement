5c5e6075066cb9b9820b9bd7e89fd34653e2dd74
Ayemi Musa <mayemij@gmail.com>
2016-04-27 19:20:32 +0100

    Forms Security configuration for Weblogic

---
+++ cosm-entitlement/src/main/java/com/nnpcgroup/cosm/controller/UserAuth.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nnpcgroup.cosm.controller;
+
+import java.io.IOException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author 18359
+ */
+@Named(value = "userAuth")
+@SessionScoped
+public class UserAuth implements Serializable {
+
+    private static final long serialVersionUID = 4727908159539105845L;
+    private static final Logger LOG = Logger.getLogger(UserAuth.class.getName());
+
+    private String username;
+    private String password;
+    private String originalURL;
+
+    /**
+     * Creates a new instance of UserAuth
+     */
+    public UserAuth() {
+    }
+
+    @PostConstruct
+    public void init() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        originalURL = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (originalURL == null) {
+            originalURL = externalContext.getRequestContextPath() + "/faces/index.xhtml";
+        } else {
+            String originalQuery = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_QUERY_STRING);
+
+            if (originalQuery != null) {
+                originalURL += "?" + originalQuery;
+            }
+        }
+    }
+
+    public void login() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+
+        try {
+            request.login(username, password);
+            LOG.log(Level.INFO, "Login successful {0}", username);
+            LOG.log(Level.INFO, "Redirecting to original url... {0}", originalURL);
+            // User user = userService.find(username, password);
+            //  externalContext.getSessionMap().put("user", user);
+            externalContext.redirect(originalURL);
+        } catch (ServletException e) {
+            // Handle unknown username/password in request.login().
+            context.addMessage(null, new FacesMessage("Unknown login"));
+            LOG.log(Level.INFO, "Unknown login {0}", username);
+        }
+    }
+
+    public void logout() throws IOException {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.invalidateSession();
+        externalContext.redirect(externalContext.getRequestContextPath() + "/faces/login.xhtml");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOriginalURL() {
+        return originalURL;
+    }
+
+    public void setOriginalURL(String originalURL) {
+        this.originalURL = originalURL;
+    }
+
+}
